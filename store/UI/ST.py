1910be74cf7bde25c4ce0d49591d359b2caa829f
Shira <shiralogasi1@gmail.com>
2024-03-26 21:35:04 +0200

    now

--- store/UI/ST.py
+++ store/UI/ST.py
@@ -3,16 +3,23 @@ import requests
 
 
 def main():
-    st.title("Welcome to my Video Gaming store")
+    st.title("Welcome to my Video Gaming store 🎮")
     st.write("Write the newest game you would like to buy:")
     new_game = st.text_input("New Game")
+    platform = st.selectbox("Select the platform:", ("PC", "Mobile", "PlayStation"))
+    main_actions = st.text_area("Main actions of the game")
+    
     if st.button("Submit"):
-        response = requests.post('http://backend:8000/games/', json={'name': new_game})
+        response = requests.post('http://backend:8000/games/', json={'name': new_game, 'platform': platform, 'main_actions': main_actions})
         if response.status_code == 200:
-            st.success("New game submitted successfully!")
+            st.success("New game submitted successfully! Maybe your game idea will be in stores soon... 🚀")
         else:
             st.error(f"An error occurred while submitting the game: {response.text}")
 
+    st.write("Feel free to share any CRAZY idea")
+
+
 if __name__ == "__main__":
     main()
+
 
